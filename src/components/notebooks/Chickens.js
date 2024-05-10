51a991f0c65e2bf980687a6a479b7da055cff022
kipkemoi Haron <kipkemoiharon4@gmail.com>
2024-05-10 11:49:32 +0300

    adding cows and chicken layout

--- src/components/notebooks/Chickens.js
+++ src/components/notebooks/Chickens.js
@@ -1,64 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import Sidebar from '../sidebar/Sidebar';
+
+
+
 
 const Chickens = () => {
-  const [chickens, setChickens] = useState([]);
+    return (
+        <div className="dashboard">
+            <Sidebar />
+                <div className="content-card">
+                    {/* Content Goes Here */}
+                    <h1>Haron</h1>
 
-  useEffect(() => {
-    fetchChickens();
-  }, []);
-
-  const fetchChickens = async () => {
-    try {
-      const response = await axios.get('http://localhost:3000/chickens');
-      setChickens(response.data);
-    } catch (error) {
-      console.error('Error fetching chickens:', error);
-    }
-  };
-
-  const handleAddChicken = async () => {
-    try {
-      const response = await axios.post('http://localhost:3000/chickens', {
-        age_months: 12,
-        weight_kg: 2.5,
-        total_eggs: 20,
-        daily_eggs: 2,
-      });
-      setChickens(prevChickens => [...prevChickens, response.data]);
-    } catch (error) {
-      console.error('Error adding chicken:', error);
-    }
-  };
-
-  const handleDeleteChicken = async (id) => {
-    try {
-      await axios.delete(`http://localhost:3000/chickens/${id}`);
-      setChickens(prevChickens => prevChickens.filter(chicken => chicken.id !== id));
-    } catch (error) {
-      console.error('Error deleting chicken:', error);
-    }
-  };
-
-  return (
-    <div>
-      <h2>My Chickens</h2>
-      <button onClick={handleAddChicken}>Add Chicken</button>
-      <ul>
-        {chickens.map(chicken => (
-          <li key={chicken.id}>
-            <p>ID: {chicken.id}</p>
-            <p>Age (months): {chicken.age_months}</p>
-            <p>Weight (kg): {chicken.weight_kg}</p>
-            <p>Total Eggs: {chicken.total_eggs}</p>
-            <p>Date: {chicken.date}</p>
-            <p>Daily Eggs: {chicken.daily_eggs}</p>
-            <button onClick={() => handleDeleteChicken(chicken.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+                    
+                </div>
+            </div>
+       
+    );
 };
 
 export default Chickens;
