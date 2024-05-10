717c92d6102916e9b0db8ae7c2f2eda80526a692
kipkemoi Haron <kipkemoiharon4@gmail.com>
2024-05-10 12:19:50 +0300

    adding chicken route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from "react";
 import Login from "./components/auth/login/Login";
 import Register from "./components/auth/register/Register";
 import Navbar from "./components/navbar/Navbar";
@@ -9,47 +8,12 @@ import Dashboard from "./components/home/Dashboard";
 import Services from "./components/services/Services";
 import Notebooks from "./components/notebooks/Notebooks";
 import Market from "./components/market/Market";
-import Chickens from "./components/notebooks/Chickens";
-import Cows from "./components/notebooks/Cows";
-
-const Notebook = ({ title }) => (
-  <div className="dashboard">
-    <Navbar />
-    <div className="content-card">
-      <h1>{title}</h1>
-    </div>
-  </div>
-);
-
-// Initial list of notebooks with general Notebook component
-const initialNotebooks = [
-  {
-    title: "Chickens",
-    path: "/chickens",
-    component: <Notebook title="Chickens" />,
-  },
-  {
-    title: "Cows",
-    path: "/cows",
-    component: <Notebook title="Cows" />,
-  },
-];
+import Chickens from "./components/notebooks/Chickens.js"
+import Cows from "./components/notebooks/Cows"
 
 function App() {
-  const [notebooks, setNotebooks] = useState(initialNotebooks);
-
-  // Function to add a new notebook
-  const addNotebook = (title) => {
-    const newPath = /${title.toLowerCase()};
-    const newNotebook = {
-      title,
-      path: newPath,
-      component: <Notebook title={title} />,
-    };
-    setNotebooks([...notebooks, newNotebook]);
-  };
-
   return (
+
     <AuthProvider>
       <Router>
         <Navbar />
@@ -60,16 +24,15 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/services" element={<Services />} />
-          <Route path="/notebooks" element={<Notebooks addNotebook={addNotebook} />} />
+          <Route path="/notebooks" element={<Notebooks />} />
+          <Route path="/chickens" element={<Chickens />} />
+          <Route path="/cows" element={<Cows />} />
           <Route path="/market" element={<Market />} />
 
-          {/* Add routes for existing notebooks */}
-          {notebooks.map((notebook, index) => (
-            <Route key={index} path={notebook.path} element={notebook.component} />
-          ))}
         </Routes>
       </Router>
     </AuthProvider>
+  
   );
 }
 
